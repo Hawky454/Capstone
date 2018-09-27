e65faf3acd7228e6ebb0c615442212ecf56552cd
David Miller <hawky454@gmail.com>
2018-09-27 16:42:01 -0500

    not happy with where things are but I need to stop and work on the server side and work out the style kinks once I get everything else working

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Cellar from './components/Cellar/Cellar';
 import Pipes from './components/Pipes/Pipes';
-import SignIn from './components/SignIn/SignIn'
+import SignIn from './components/SignIn/SignIn';
+
+
+
 
 ReactDOM.render(
+
     <Router>
         <div>
             <Route exact path="/" component={App} /> 
@@ -20,6 +24,6 @@ ReactDOM.render(
     </Router>, document.getElementById('root')
 );
 
-registerServiceWorker();
+
 
 // ReactDOM.render(<App />, document.getElementById('root'));
