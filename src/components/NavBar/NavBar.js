7f1f3b31fb321a05c96b062eb60f6ec1128ed508
David Miller <hawky454@gmail.com>
2018-09-28 16:53:08 -0500

    finishing up at school got everything working again and i generated an express server app in another directory and im going to keep it seperate from this app

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,12 +7,12 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => {
     return(
         <div className={classes.NavBar}>
-          <Link to="/"><a className={classes.left}>The Tobacco Cellar</a></Link>
-          <Link to="/Cellar"><a className={classes.left}>Cellar</a></Link>
-          <Link to="/Pipes"><a className={classes.left}>Pipes</a></Link>
+          <Link to="/"><span className={classes.left}>The Tobacco Cellar</span></Link>
+          <Link to="/Cellar"><span className={classes.left}>Cellar</span></Link>
+          <Link to="/Pipes"><span className={classes.left}>Pipes</span></Link>
           <section>
-            <Link to="/SignUp"><a className={classes.right}>Sign Up</a></Link>
-            <Link to="/SignIn"><a onClick={() => props.clicked} className={classes.right}>Sign In</a></Link>
+            <Link to="/SignUp"><span className={classes.right}>Sign Up</span></Link>
+            <Link to="/SignIn"><span onClick={() => props.clicked} className={classes.right}>Sign In</span></Link>
           </section>
         </div>
     );
