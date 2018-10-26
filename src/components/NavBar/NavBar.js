607c569777052d736a62fbb92236a52fe2398fdc
David Miller <hawky454@gmail.com>
2018-10-25 23:56:08 -0500

    still have a lot of work to do need to figure out how to get the hamburger menu up and running but Im starting to get the hang of these @medai querries

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,8 +11,10 @@ const NavBar = (props) => {
           <Link to="/CellarList"><span className={classes.left}>Cellar</span></Link>
           <Link to="/Pipes"><span className={classes.left}>Pipes</span></Link>
           <section>
+          <div className={classes.rightMediaQuery}>
             <Link to="/SignUp"><span className={classes.right}>Sign Up</span></Link>
             <Link to="/SignIn"><span onClick={() => props.clicked} className={classes.right}>Log In</span></Link>
+          </div>
           </section>
         </div>
     );
